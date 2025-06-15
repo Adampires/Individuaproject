7dcda6f9f3098bf1d924300ba947067b5e056539
adampires <nurhakim2119@gmail.com>
2025-06-15 19:42:07 +0800

    tukar link String url = "https://github.com/Adampires/Individuaproject.git";

--- app/src/main/java/com/example/individuaproject/Estimate.java
+++ app/src/main/java/com/example/individuaproject/Estimate.java
@@ -7,12 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Estimate extends AppCompatActivity {
 
-    Spinner applianceSpinner, hourSpinner;
-    TextView resultKwh, resultCost;
+    Spinner applianceSpinner, hourSpinner, daySpinner;
+    TextView resultPerHour, resultKwh, resultCost;
     Button backButton;
 
     // Appliance power ratings in Watts
-    final int[] applianceWatts = {75, 1500, 200, 500}; // Fan, Aircond, Fridge, Washing Machine
+    final int[] applianceWatts = {75, 1500, 200, 500};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +21,21 @@ public class Estimate extends AppCompatActivity {
 
         applianceSpinner = findViewById(R.id.applianceSpinner);
         hourSpinner = findViewById(R.id.hourSpinner);
+        daySpinner = findViewById(R.id.daySpinner);
+        resultPerHour = findViewById(R.id.resultPerHour);
         resultKwh = findViewById(R.id.resultKwh);
         resultCost = findViewById(R.id.resultCost);
         backButton = findViewById(R.id.backBtn);
 
         String[] appliances = {"Fan", "Aircond", "Fridge", "Washing Machine"};
         String[] hours = new String[24];
+        String[] days = new String[31];
         for (int i = 0; i < 24; i++) hours[i] = String.valueOf(i + 1);
+        for (int i = 0; i < 31; i++) days[i] = String.valueOf(i + 1);
 
-        ArrayAdapter<String> applianceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, appliances);
-        applianceSpinner.setAdapter(applianceAdapter);
-
-        ArrayAdapter<String> hourAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, hours);
-        hourSpinner.setAdapter(hourAdapter);
+        applianceSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, appliances));
+        hourSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, hours));
+        daySpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, days));
 
         AdapterView.OnItemSelectedListener selectionListener = new AdapterView.OnItemSelectedListener() {
             @Override
@@ -47,19 +49,24 @@ public class Estimate extends AppCompatActivity {
 
         applianceSpinner.setOnItemSelectedListener(selectionListener);
         hourSpinner.setOnItemSelectedListener(selectionListener);
+        daySpinner.setOnItemSelectedListener(selectionListener);
 
         backButton.setOnClickListener(v -> finish());
     }
 
     private void calculateEstimate() {
         int applianceIndex = applianceSpinner.getSelectedItemPosition();
-        int hour = Integer.parseInt(hourSpinner.getSelectedItem().toString());
+        int hours = Integer.parseInt(hourSpinner.getSelectedItem().toString());
+        int days = Integer.parseInt(daySpinner.getSelectedItem().toString());
 
         double watt = applianceWatts[applianceIndex];
-        double kwh = (watt * hour) / 1000.0;
-        double cost = kwh * 0.218; // Use first block rate
+        double kwhPerHour = watt / 1000.0;
+        double dailyKwh = kwhPerHour * hours;
+        double monthlyKwh = dailyKwh * days;
+        double cost = monthlyKwh * 0.218;
 
-        resultKwh.setText(String.format("Estimated kWh: %.2f", kwh));
-        resultCost.setText(String.format("Estimated Cost: RM %.2f", cost));
+        resultPerHour.setText(String.format("kWh Per Hour: %.3f", kwhPerHour));
+        resultKwh.setText(String.format("Monthly Estimated kWh: %.2f", monthlyKwh));
+        resultCost.setText(String.format("Monthly Estimated Cost: RM %.2f", cost));
     }
 }
